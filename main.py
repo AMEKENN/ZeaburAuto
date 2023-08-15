38f97ba3312160652cd591d2898a91205c84735e
ERA-YES <ERA_YES@outlook.com>
2023-08-15 13:49:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 driver.get("https://dash.zeabur.com/login")
 
-driver.find_element(By.LINK_TEXT, "Sign in with Github").click()
+driver.find_element(By.CSS_SELECTOR, "#root>div>div>div>div>div>div>a>div").click()
 driver.find_element(By.ID, "login_field").sendkeys(os.environ['USERNAME'])
 driver.find_element(By.ID, "password").sendkeys(os.environ['PASSWORD'])
 driver.find_element(By.NAME, "commit").click()
-driver.find_element(By.LINK_TEXT, "Days").click()
-driver.find_element(By.LINK_TEXT, "Extend Trial").click()
+driver.find_element(By.CSS_SELECTOR, "#root>div>div>div>div.overflow-auto>div>div>div.flex").click()
+driver.find_element(By.CSS_SELECTOR, "#headlessui-dialog-panel-\:r2\:>div:nth-child(4)>div>button").click()
 
 time.sleep(10)
 
