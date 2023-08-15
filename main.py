e31d46b168c1488f201a2ad7810f74002c4af0e2
EraZeabur <142290099+EraZeabur@users.noreply.github.com>
2023-08-15 13:25:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+driver.get("https://dash.zeabur.com/login")
+
+driver.find_element(By.LINK_TEXT, "Sign in with Github").click()
+driver.find_element(By.ID, "login_field").sendkeys(os.environ['USERNAME'])
+driver.find_element(By.ID, "password").sendkeys(os.environ['PASSWORD'])
+driver.find_element(By.NAME, "commit").click()
+driver.find_element(By.LINK_TEXT, "Days").click()
+driver.find_element(By.LINK_TEXT, "Extend Trial").click()
+
+time.sleep(10)
+
+driver.quit()
