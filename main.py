349bcd64d5dac38f6842ef6332348ac9ef9875a6
ERA-YES <ERA_YES@outlook.com>
2023-08-15 13:32:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-chrome_options = Options()
+chrome_options = options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 driver.get("https://dash.zeabur.com/login")
+
 driver.find_element(By.LINK_TEXT, "Sign in with Github").click()
 driver.find_element(By.ID, "login_field").sendkeys(os.environ['USERNAME'])
 driver.find_element(By.ID, "password").sendkeys(os.environ['PASSWORD'])
